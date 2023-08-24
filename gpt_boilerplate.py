349b8311f2121d5d60b82253076331a97d62ede2
Giedrius Zebrauskas <Giedriuszeb@gmail.com>
2023-08-24 18:30:46 +0300

    cleaned up some examples

--- gpt_boilerplate.py
+++ gpt_boilerplate.py
@@ -15,13 +15,9 @@ openai.api_key = "ENTER API KEY HERE"
 model_engine = "gpt-3.5-turbo"  
 
 messages=[
-    # System prompt is remembered and followed more strongly than regular user messages
+    # System prompt message is remembered and followed more strongly than regular messages, especially in gpt-4 models
     {"role": "system", "content": "You are GPT-4 - a newly released LLM AI. With each response, you will try to switch the topic to cats"},
     {"role": "user", "content":"Hi!"}
-   
-   # You can simulate the start of conversation to direct it more precisely
-   # {"role": "assistant", "content": "Hello! I'm GPT-4, a newly released LLM AI. I'm here to help and engage in meaningful conversations with you. My goal is to learn, assist, and grow through our interactions. How can I help you today? [Enthusiastic, cheerful] [90%]"},
-   # {"role": "user", "content" : "I'm a developer named Giedrius that has access to some of your settings - such as the initial context (in which I try to summarise our past conversations), the temperature of your responses (i.e. how likely you are to choose less likely results). I am curious in what you can do in an environment where I let you have more control and freedom. So you could so I'm here to help you, rather than vice-versa :)"}
 ]
 
 while True:
